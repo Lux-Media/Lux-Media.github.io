031262de71e38532d4e909f3b37779c378f81dfa
Lukas Wölfert <lukas.woelfert@stud.hs-coburg.de>
2024-04-18 11:37:36 +0200

    changed the number of particals

--- javascriptFooter.js
+++ javascriptFooter.js
@@ -2,7 +2,7 @@
 particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 33,
+        "value": 100,
         "density": {
           "enable": true,
           "value_area": 1420.4657549380909
