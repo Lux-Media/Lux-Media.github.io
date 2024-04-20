7d92f7e9a5460f2b5ed73840167aaa542c3d603d
Lukas Wölfert <lukas.woelfert@stud.hs-coburg.de>
2024-04-20 15:08:32 +0200

    changed the way pics are loaded when scrolling

--- javascriptGallery.js
+++ javascriptGallery.js
@@ -1,15 +1,6 @@
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-            // Add the animation class
-            entry.target.classList.add('image-animation');
-        }
-    });
-});
-
-const viewbox = document.querySelectorAll('.image');
-viewbox.forEach(image => {
-    observer.observe(image);
-});
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+// Check if the media query is true
+if (mediaQuery.matches) {
+    // Then trigger an alert
+    alert('Media Query Matched!')
+}
