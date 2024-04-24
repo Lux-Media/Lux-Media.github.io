7b63b06e9ac05f7653aa785c5207df9c5eec571e
Lukas Wölfert <lukas.woelfert@stud.hs-coburg.de>
2024-04-24 11:06:13 +0200

    ads a zoom indicator to the full screen gallery

--- javascript/Gallery.js
+++ javascript/Gallery.js
@@ -40,3 +40,30 @@ mmObj.addEventListener('change', function () {
     mediasize(mmObj);
 });
 
+
+//adds the function to open the images in fullscreen when clicked on. it uses photoswipe to do this https://photoswipe.com/options/
+import PhotoSwipeLightbox from 'https://cdn.jsdelivr.net/npm/photoswipe@5.3.7/dist/photoswipe-lightbox.esm.js';
+const options = {
+  gallery: '#my-gallery',
+  children: 'a',
+  pswpModule: () => import('https://cdn.jsdelivr.net/npm/photoswipe@5.3.7/dist/photoswipe.esm.js'),
+  preload: [1, 4],
+  rrowKeys: true,
+  escKey: true,
+  wheelToZoom: true,
+};
+const lightbox = new PhotoSwipeLightbox(options);
+lightbox.on('uiRegister', function () {
+  lightbox.pswp.ui.registerElement({
+    name: 'zoom-level-indicator',
+    order: 9,
+    onInit: (el, pswp) => {
+      pswp.on('zoomPanUpdate', (e) => {
+        if (e.slide === pswp.currSlide) {
+          el.innerText = 'Zoom level is ' + Math.round(pswp.currSlide.currZoomLevel * 100) + '%';
+        }
+      });
+    }
+  });
+});
+lightbox.init();
